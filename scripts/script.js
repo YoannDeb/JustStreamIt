a58249521e8a79b8d31d935d30754806ea631e49
YoannDeb <yoann.deblois@gmail.com>
2021-09-05 20:45:17 +0200

    style: removed useless spaces

--- scripts/script.js
+++ scripts/script.js
@@ -1,127 +1,55 @@
-//fetch examples for API OCMovies:
-//fetch("http://localhost:8000/api/v1/titles/?format=json&sort_by=-imdb_score");
-//fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=music");
-//fetch("http://localhost:8000/api/v1/titles/499549")
+const baseURL = "http://localhost:8000/api/v1/titles/?format=json&sort_by=-imdb_score&genre="
 
-function bestFilm(){
-    fetch("http://localhost:8000/api/v1/titles/?format=json&sort_by=-imdb_score")
-        .then(function(resolution){
-            if(resolution.ok){
-                return resolution.json();
+async function fetchMovies(genre, quantity){
+    let movies = []
+    let response = await fetch(baseURL + genre);
+    let json = await response.json();
+    console.log(json)
+    for (let result of await json.results){
+        if (movies.length < quantity){
+            movies.push(result);
+        }
+    }
+    while (movies.length < quantity){
+        response = await fetch(await json.next);
+        json = await response.json();
+        for (let result of await json.results){
+            if (movies.length < quantity){
+                movies.push(result);
             }
-        })
-        .then(function(json){
-            return json.results[0].id;
-    })
-    .then(function(id){
-        fetch("http://localhost:8000/api/v1/titles/" + id + "?format=json")
-        .then(function(resolution){
-            if(resolution.ok){
-                return resolution.json();
-            }
-        })
-        .then(function(json){
-            document.querySelector("#best_film h1").innerText = json.title;
-            document.querySelector("#best_film img").setAttribute("src", json.image_url);
-            document.querySelector("#best_film p").innerText = json.description;
-        })
-    })
-    .catch(function(e){
-        console.error(e);
-    });
+        }
+    }
+    return movies;
 }
 
-// async function bestFilmDescription(){
-//     const id = await bestFilmId()
-//     console.log("http://localhost:8000/api/v1/titles/" + id + "/?format=json")
-// }
-
-async function bestFilmsIdsPage1(genre){
-    let ids = [];
-    await fetch("http://localhost:8000/api/v1/titles/?format=json&sort_by=-imdb_score&genre=" + genre)
-        .then(function(resolution){
-            if(resolution.ok){
-                return resolution.json();
-            }
-        })
-        .then(function(json){
-            for(result of json.results){
-                ids.push(result.id);
-            }
-    })
-    .catch(function(e){
-        console.error(e);
-    });
-    return ids;
+async function fetchMovieDetailled(movie){
+    let response = await fetch(movie.url);
+    return await response.json();
 }
 
-async function bestFilmsIdsPage2(genre){
-    let ids = [];
-    await fetch("http://localhost:8000/api/v1/titles/?format=json&page=2&sort_by=-imdb_score&genre=" + genre)
-        .then(function(resolution){
-            if(resolution.ok){
-                return resolution.json();
-            }
-        })
-        .then(function(json){
-            for(let i = 0; i < 2; i++){
-                ids.push(json.results[i].id);
-            }
-    })
-    .catch(function(e){
-        console.error(e);
-    });
-    return ids;
+async function modifyBestFilm(movieDetailled){
+    document.querySelector("#best_film h1").innerText = movieDetailled.title;
+    document.querySelector("#best_film img").setAttribute("src", movieDetailled.image_url);
+    document.querySelector("#best_film p").innerText = movieDetailled.description;
 }
 
-async function bestFilmsIds(genre){
-    let idsPage1 = await bestFilmsIdsPage1(genre);
-    let idsPage2 = await bestFilmsIdsPage2(genre);
-    return idsPage1.concat(idsPage2);
-}
-
-async function modifyCategoryFilmsImage(ids, selector){
+async function modifyCategoryFilmsImage(movies, selector){
     let links = document.querySelectorAll(selector + " a");
-    for(let i = 0; i < ids.length; i++){
-        fetch("http://localhost:8000/api/v1/titles/" + ids[i] + "?format=json")
-        .then(function(resolution){
-            if(resolution.ok){
-                return resolution.json();
-            }
-        })
-        .then(function(json){
-            links[i].innerHTML = '<img src="' + json.image_url + '" alt="' + json.title + ' poster" />'
-            links[i].setAttribute("href", json.url);
-        })
-        .catch(function(e){
-            console.error(e);
-        })
+    for(let i = 0; i < movies.length; i++){
+            links[i].innerHTML = '<img src="' + movies[i].image_url + '" alt="' + movies[i].title + ' poster" />'
+            links[i].setAttribute("href", movies[i].imdb_url);
+            links[i].setAttribute("target", "_blank");
     }
 }
 
-// let selector = "#best_films";
-// console.log(selector);
-// let links = document.querySelectorAll(selector + " a")[0].innerHTML = '<img src="img/logo.png" alt="" />';
-
-// let links = document.querySelector(selector);
-// console.log("links" + links);
-// window.addEventListener('load', function(){
-//     bestFilm();
-//     bestFilmsIds()
-//     .then(function(e){
-//         console.log(e)
-//         modifyCategoryFilmsImage(e, "#best_films")
-//     })
-//     .catch(function(e){
-//         console.error(e);
-//     });
-// });
 async function main(){
-    bestFilm();
-    modifyCategoryFilmsImage(await bestFilmsIds(""), "#best_films")
-    modifyCategoryFilmsImage(await bestFilmsIds("sci-fi"), "#Sci-Fi")
-    modifyCategoryFilmsImage(await bestFilmsIds("comedy"), "#Comedy")
-    modifyCategoryFilmsImage(await bestFilmsIds("music"), "#Music");
+    let bestFilms = await fetchMovies("", 8);
+    let bestFilm = await bestFilms.shift();
+    await modifyBestFilm(await fetchMovieDetailled(bestFilm));
+    await modifyCategoryFilmsImage(bestFilms, "#best_films")
+    await modifyCategoryFilmsImage(await fetchMovies("sci-fi", 7), "#Sci-Fi")
+    await modifyCategoryFilmsImage(await fetchMovies("comedy", 7), "#Comedy")
+    await modifyCategoryFilmsImage(await fetchMovies("music", 7), "#Music");
 }
 
 main()
