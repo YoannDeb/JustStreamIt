acc287af706de59952b7e18e5fbd7a7ed765bc5d
Yoann <yoann.deblois@gmail.com>
2021-09-16 11:29:52 +0200

    docs: Docstrings Done.

--- scripts/api.js
+++ scripts/api.js
@@ -1,6 +1,13 @@
 const baseURL = "http://localhost:8000/api/v1/titles/?format=json&sort_by=-imdb_score&genre="
 
-async function buildMovieList(json, movies, quantity){
+/**
+ * Completes movie list until the end of the movies provided or
+ * the list lenght is equal to the quantity desired.
+ * @param {object}  json      Parsed response of the API.
+ * @param {table}   movies    A table of Movies object we want to fill.
+ * @param {number}  quantity  Quantity of movies desired in the final list.
+ */
+async function completeMovieList(json, movies, quantity){
     for (let result of await json.results){
         if (movies.length < quantity){
             movies.push(result);
@@ -9,25 +16,30 @@ async function buildMovieList(json, movies, quantity){
 }
 
 /**
- * Makes a list with objects fetched from API server.
- * Depends of genre and number of films required.
- * @param {string} genre: If genre is an empty string, API will return best films in all categories.
- * @param {number} quantity: quantity of films desired in the final list.
- * @returns a list of movies
+ * Makes a list with movies' objects fetched from API server.
+ * Depends of genre and number of movies required.
+ * @param {string} genre     If genre is an empty string, API will return best movies in all categories.
+ * @param {number} quantity  Quantity of movies desired in the final list.
+ * @returns                  A list of movies' objects.
  */
 export async function fetchMovies(genre, quantity){
     let movies = []
     let response = await fetch(baseURL + genre);
     let json = await response.json();
-    buildMovieList(json, movies, quantity);
+    completeMovieList(json, movies, quantity);
     while (movies.length < quantity){
         response = await fetch(await json.next);
         json = await response.json();
-        buildMovieList(json, movies, quantity);
+        completeMovieList(json, movies, quantity);
     }
     return movies;
 }
 
+/**
+ * Take a movie object and return an other movie object, more detailed, from the API.
+ * @param {object} movie  A movie's object.
+ * @returns               A movies' object but more detailed.
+ */
 export async function fetchMovieDetailed(movie){
     let response = await fetch(movie.url);
     return await response.json();
